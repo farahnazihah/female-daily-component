48d238f2b66304c944219d8669ccdcc7b000d730
Farah Nazihah <farahnazihah102@gmail.com>
2023-02-11 20:34:26 +0700

    add images source host

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.femaledaily.com",
+        port: "",
+        pathname: "/*",
+      },
+    ],
+  },
+};
